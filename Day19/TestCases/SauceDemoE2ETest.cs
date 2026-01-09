c5d9be0d50254a49c22e6f80ea2e417a791f2545
Habib Siam <habib.siam.work@gmail.com>
2026-01-09 16:33:49 +0600

    Refactor SauceDemoE2ETest for improved clarity

--- Day19/TestCases/SauceDemoE2ETest.cs
+++ Day19/TestCases/SauceDemoE2ETest.cs
@@ -1,4 +1,4 @@
-﻿using Day19.Config;
+using Day19.Config;
 using Day19.DataReaders;
 using Day19.PageMethods;
 using Day19.TestData;
@@ -23,54 +23,48 @@ namespace Day19.TestCases
             test.Info("Navigating to SauceDemo");
             login.Navigate();
 
-            test.Info($"Logging in as  username:{data.Username}, password: {data.Password}");
+            test.Info($"Logging in as username: {data.Username}");
             login.Login(data.Username, data.Password);
 
             WebDriverWait wait = new WebDriverWait(GetDriver(), TimeSpan.FromSeconds(10));
             wait.Until(d =>
-            {
- 
-                return d.Url.Contains("inventory") ||
-                       d.FindElements(By.CssSelector("[data-test='error']")).Count > 0;
-            });
+                d.Url.Contains("inventory") ||
+                d.FindElements(By.CssSelector("[data-test='error']")).Count > 0
+            );
 
-            if (!driver.Url.Contains("inventory"))
+            if (!GetDriver().Url.Contains("inventory"))
             {
                 test.Fail("Login failed");
                 Assert.Fail("Login failed");
             }
 
-
-
-            if (data.Items.Count > 0)
+            if (data.Items != null && data.Items.Count > 0)
             {
                 test.Info("Adding products");
-                for (int i = 0; i < data.Items.Count; i++)
+                foreach (var item in data.Items)
                 {
-                    test.Info();
-                    products.AddToCart(data.Items[i]);
+                    test.Info($"Adding item: {item}");
+                    products.AddToCart(item);
                 }
 
-
-
-                //Thread.Sleep(2000);
                 test.Info("Opening cart");
                 products.OpenCart();
-                //Thread.Sleep(2000);
+
                 test.Info("Checkout");
                 cart.Checkout();
-                //Thread.Sleep(2000);
+
                 test.Info("Filling checkout info");
                 checkout.FillInfo("Habib", "Siam", "1234");
-                //Thread.Sleep(2000);
+
                 test.Info("Finishing order");
                 checkout.Finish();
                 checkout.BackToProducts();
-                //Thread.Sleep(2000);
+
                 test.Info("Logging out");
                 checkout.Logout();
             }
         }
+
 
         public static IEnumerable<LoginData> LoginDataFromJson()
         {
