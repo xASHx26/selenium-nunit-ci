18cd474d2146c518d1f0edc847d14308a9741681
Habib Siam <habib.siam.work@gmail.com>
2026-01-09 17:07:20 +0600

    Refactor SauceDemoE2ETest for clarity and structure

--- Day19/TestCases/SauceDemoE2ETest.cs
+++ Day19/TestCases/SauceDemoE2ETest.cs
@@ -5,6 +5,7 @@ using Day19.TestData;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 
 namespace Day19.TestCases
@@ -23,7 +24,7 @@ namespace Day19.TestCases
             test.Info("Navigating to SauceDemo");
             login.Navigate();
 
-            test.Info($"Logging in as username: {data.Username}");
+            test.Info($"Logging in as: {data.Username}");
             login.Login(data.Username, data.Password);
 
             WebDriverWait wait = new WebDriverWait(GetDriver(), TimeSpan.FromSeconds(10));
@@ -32,13 +33,14 @@ namespace Day19.TestCases
                 d.FindElements(By.CssSelector("[data-test='error']")).Count > 0
             );
 
+            // ❌ Invalid users → Skip (EXPECTED)
             if (!GetDriver().Url.Contains("inventory"))
             {
                 test.Info("Login failed as expected for invalid user");
                 Assert.Ignore("Skipping E2E flow for invalid login");
             }
 
-
+            // ✅ Valid user flow
             if (data.Items != null && data.Items.Count > 0)
             {
                 test.Info("Adding products");
@@ -65,7 +67,6 @@ namespace Day19.TestCases
                 checkout.Logout();
             }
         }
-
 
         public static IEnumerable<LoginData> LoginDataFromJson()
         {
